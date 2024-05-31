3ff01d1a71722467e2d2805c612815645fa52399
Awake-m <jagrutimakwana124@gmail.com>
2024-05-31 10:49:34 +0530

    app1

---
+++ app1/src/App.js
@@ -0,0 +1,224 @@
+import logo from './logo.svg';
+// import './App.css';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          {/* <h1>Jagruti</h1> */}
+
+
+          <div id="header">
+               <div id="logo">
+                <br></br>
+                <img src="https://preview.colorlib.com/theme/pillowmart/img/logo.png"/>
+               </div>
+               <nav>
+                  <ul>
+                      <li>Home</li>
+                      <li>About</li>
+                      <li>Product</li>
+                      <li>Page</li>
+                      <li>Blog</li>
+                      <li>Contact</li>
+                  </ul>
+               </nav>
+          </div>
+
+         <div id="home">
+              <div id="part-1"></div>
+              <div id="part-2">
+                <h1 class="">Best quality pillow</h1>
+                <p class="">Seamlessly empower fully researched growth strategies and interoperable internal</p><br></br>
+                 <button id="btn1">Shop Now</button>
+              </div>
+
+              <div id="part-3">
+              <img src="https://preview.colorlib.com/theme/pillowmart/img/banner.png" />
+             </div>
+         </div>
+
+
+
+
+         <div id="section-2">
+            <div id="box1">             
+              <img src="https://preview.colorlib.com/theme/pillowmart/img/single_product_1.png"  />
+            </div>
+            <div id="box">
+              <p>Started from $10</p>
+              <h1>Printed memory foam <br></br>  brief modern throw <br></br>  pillow case</h1>
+               <button id="btn2">Explore Now</button>
+            </div>
+        </div>
+
+        <div class="container" id="section-3">
+        
+            <div id="box11">
+              <p>Started from $10</p>
+              <h1>Printed memory foam <br></br> brief modern throw <br></br> pillow case</h1>
+               <button id="btn2">Explore Now</button>
+            </div>
+
+            <div id="box12" class="">             
+              <img src="https://preview.colorlib.com/theme/pillowmart/img/single_product_2.png" />
+            </div>           
+       </div>
+       
+
+       <div class="container" id="section-2">
+
+            <div id="box1" class="">             
+              <img src="https://preview.colorlib.com/theme/pillowmart/img/single_product_3.png" class="ms-5 mt-5" />
+            </div>
+            <div id="box">
+              <p>Started from $10</p>
+              <h1>Printed memory foam<br></br> brief modern throw <br></br>  pillow case</h1>
+               <button id="btn2">Explore Now</button>
+            </div>
+      </div><br></br><br></br>
+
+
+
+      <div id="section-5">
+          <h1>Trending Items</h1><br></br><br></br>
+          <div class="main">
+            <div id="trading">
+              <img src="https://preview.colorlib.com/theme/pillowmart/img/tranding_item/tranding_item_1.png" />
+              <h3>Cervical pillow for airplane<br></br> car office nap pillow</h3>
+              <p>From $5</p>
+            </div>
+
+             <div id="trading">
+              <img src="https://preview.colorlib.com/theme/pillowmart/img/tranding_item/tranding_item_2.png" />
+              <h3>Cervical pillow for airplane<br></br> car office nap pillow</h3>
+              <p>From $5</p>
+            </div>
+
+             <div id="trading">
+              <img src="https://preview.colorlib.com/theme/pillowmart/img/tranding_item/tranding_item_3.png" />
+              <h3>Cervical pillow for airplane<br></br> car office nap pillow</h3>
+              <p>From $5</p>
+            </div>
+             <div id="trading">
+              <img src="https://preview.colorlib.com/theme/pillowmart/img/tranding_item/tranding_item_4.png" />
+              <h3>Cervical pillow for airplane<br></br> car office nap pillow</h3>
+              <p>From $5</p>
+            </div>
+
+             <div id="trading">
+              <img src="https://preview.colorlib.com/theme/pillowmart/img/tranding_item/tranding_item_5.png" />
+              <h3>Cervical pillow for airplane<br></br> car office nap pillow</h3>
+              <p>From $5</p>
+            </div>
+
+
+             <div id="trading">
+              <img src="https://preview.colorlib.com/theme/pillowmart/img/tranding_item/tranding_item_6.png" />
+              <h3>Cervical pillow for airplane<br></br> car office nap pillow</h3>
+              <p>From $5</p>
+            </div>
+
+          </div>
+      </div>
+
+      
+
+      <div id="section-7">
+      <br></br> <br></br> <br></br>
+          <div class="container1">
+           
+              <div id="blog">
+                <h1>Credibly innovate granular<br></br> internal or organic sources<br></br> whereas standards.</h1>
+              </div>
+              <div>
+                <p class="mt-5 text-secondary" style={{"color" : "gray"}}>Seamlessly empower fully researched growth strategies <br></br> and interoperable internal or “organic” sources. Credibly innovate <br></br> granular internal or “organic” sources whereas high standards in <br></br> web-readiness.</p>
+              </div>
+          </div>
+          <div id="blog2">
+              <div class="border">
+                <img src="https://preview.colorlib.com/theme/pillowmart/img/icon/feature_icon_1.svg" class="ms-5" />
+                <p>Credit Card Support</p>
+              </div>
+
+              <div class="border">
+                <img src="https://preview.colorlib.com/theme/pillowmart/img/icon/feature_icon_2.svg" class="ms-5"/>
+                <p>Online Order</p>
+              </div>
+
+              <div class="border">
+                <img src="https://preview.colorlib.com/theme/pillowmart/img/icon/feature_icon_3.svg" class="ms-5"/>
+                <p>Free Delivery</p>
+              </div>
+
+              <div class="border">
+                <img src="https://preview.colorlib.com/theme/pillowmart/img/icon/feature_icon_4.svg" class="ms-5"/>
+                <p>Product with Gift</p>
+              </div>
+          </div>
+    </div>
+
+
+
+
+    <div id="section-8" >
+              <header>
+          <nav class="navbar navbar-expand-lg container">
+  <div class="container2">
+    <div class="imgbox">
+          <img src="https://preview.colorlib.com/theme/pillowmart/img/logo.png" /></div>
+    <div  id="lastnav">
+      <ul>
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>About</a>
+        </li>
+        <li>
+          <a >
+            Product
+          </a>
+        </li>
+        <li>
+          <a >
+            Page
+          </a>
+        </li>
+        <li>
+          <a >
+            Blog
+          </a>
+        </li>
+        <li>
+          <a>Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+      </header>
+      
+        </div>
+
+
+
+      
+
+        <div id="section-9">
+          <h5 style = {{"margin-top" : "20px"}}>Copyright ©2024 All rights reserved | This template is made with <i class="bi bi-suit-heart"></i> by Jagruti Makwana.</h5>
+          </div>
+
+      
+
+         
+         
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
