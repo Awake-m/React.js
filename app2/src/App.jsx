d8a7ea502020447ae280e8af31eb956e7c8fd4bc
Awake-m <jagrutimakwana124@gmail.com>
2024-06-04 11:53:25 +0530

    app2

--- app2/src/App.jsx
+++ app2/src/App.jsx
@@ -1,85 +1,7 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import {useState} from 'react';
-
-
-// function App() {
-  
-//   let [count, setCount] = useState(0);
-//   let [disable, setDisable] = useState(true);
-
-//   const Inc = () => {
-//     setCount(count + 1);
-//     setDisable(false);
-//   };
-//   const dec = () => {
-//     if (count > 1) {
-//       setCount(count - 1);
-//     } else {
-
-
-//       setCount(0);
-//       setDisable(true);
-//     }
-
-     
-
-//     const [votes, setVotes] = useState({ option1: 0, option2: 0 });
-
-//   const handleVote = (option) => {
-//     setVotes((prevVotes) => ({
-//       ...prevVotes,
-//       [option]: prevVotes[option] + 1,
-//     }));
-    
-//   };
-
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-      
-//       <h1>Couter Up</h1>
-//       <div className="box1">
-//       <h3>{count}</h3>
-//       <button onClick={Inc}>+</button>
-
-      
-//       <button disabled={disable} onClick={dec}>
-//         -
-//       </button>
-
-
-//       <div className="box2">
-//       <h1>Simple Voting App</h1>
-//       <div>
-//         <h2>Option 1</h2>
-//         <button onClick={() => handleVote('option1')}>Vote</button>
-//         <p>Votes: {votes.option1}</p>
-//       </div>
-//       <div>
-//         <h2>Option 2</h2>
-//         <button onClick={() => handleVote('option2')}>Vote</button>
-//         <p>Votes: {votes.option2}</p>
-//       </div>
-//       </div>
-
-//     </div>
-
-    
-
-//       </header>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
 
 import React, { useState } from 'react';
 import './App.css';
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -130,6 +52,7 @@ function App() {
           <div className="option">
             <h2>Option-2</h2>
             <button onClick={() => handleVote('option2')}>Vote</button>
+          
             <p>Votes: {votes.option2}</p>
           </div>
         </div>
